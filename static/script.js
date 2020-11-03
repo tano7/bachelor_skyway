44ddfca0917ab63ac893133490baa39748192e19
tano7 <tano.16.7.4@gmail.com>
2020-11-03 20:46:50 +0900

    相互注視時に目を黄色く光らせる

--- static/script.js
+++ static/script.js
@@ -116,6 +116,11 @@ let finalTranscript = ''; // 確定した(黒の)認識結果
     //初めて繋がった時にメッセージ送る
     dataConnection.once('open', async () => {
       messages.textContent += `=== DataConnection has been opened ===\n`;
+      //ws.sendで取得した値を動的に取得するためにwebsocketに送信するデータを格納する命令を出す
+      message=3;
+      ws.send(message);
+      //sendボタン破棄した
+      //sendTrigger.addEventListener('click', onClickSend); //繋がったらsendボタンを有効にしているみたい
     });
 
     //setInterval(ToPython, 350);
@@ -256,6 +261,9 @@ let finalTranscript = ''; // 確定した(黒の)認識結果
     peer.on('connection', dataConnection => {
       dataConnection.once('open', async () => {
         messages.textContent += `=== DataConnection has been opened ===\n`;
+        message=3;
+        ws.send(message);
+        //sendTrigger.addEventListener('click', onClickSend); ここコメントアウトして良かったか試してみないと
       });
 
       dataConnection.on('data', data => {
