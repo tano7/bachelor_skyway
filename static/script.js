4564dc73a13811ac02f9401596b5661747e15090
tano7 <tano.16.7.4@gmail.com>
2020-11-28 21:26:14 +0900

    調整完了

--- static/script.js
+++ static/script.js
@@ -182,6 +182,7 @@ let finalTranscript = ''; // 確定した(黒の)認識結果
       }
 
       ws.send(data); //Pythonにリモートデータ送信
+      console.log(last_time);
       //console.log(callJudge);
       //    console.log(data);
     }
@@ -339,7 +340,7 @@ let finalTranscript = ''; // 確定した(黒の)認識結果
             data[4] = 'g';
           }
           ws.send(data); //pythonにリモートデータ送信
-          //console.log(callJudge);
+          console.log(last_time);
           //console.log(data);
         }
       });
